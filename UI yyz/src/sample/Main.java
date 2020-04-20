23f821e5cbf0347e8b6b556533ca0f46ca502d59
JingWei666 <woshidiyihaha@gamil.com>
2020-04-20 14:11:10 +0100

    分好类别

--- UI yyz/src/sample/Main.java
+++ UI yyz/src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("SearchPage.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/TagsPage.fxml"));
+        primaryStage.setTitle("GROUP 'ASAP'");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
