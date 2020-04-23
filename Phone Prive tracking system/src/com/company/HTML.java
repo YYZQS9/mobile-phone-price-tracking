76b5594a30a02b51b321f55de5b4944860cdf6ec
yang981021 <47185706+yang981021@users.noreply.github.com>
2020-04-23 17:11:39 +0100

    Update HTML.java

--- Phone Prive tracking system/src/com/company/HTML.java
+++ Phone Prive tracking system/src/com/company/HTML.java
@@ -5,29 +5,140 @@ public class HTML {
 
     public static String html(String key){
 
-        if(key.equals("XS")) {
-            String IphoneXS = new String("https://www.currys.co.uk/gbuk/phones-broadband-and-sat-nav/mobile-phones-and-accessories/mobile-phones/apple-iphone-se-64-gb-black-10204868-pdt.html");
-            return IphoneXS;
+        if(key.equals("IphoneXS")) {
+
+            //Argos iPhone XS。
+            Document doc = Jsoup.parse(new URL("https://www.argos.co.uk/list/shop-for-iphone-xs-range?tag=ar:iphoneriverpage"), 100000);
+            Elements element = doc.select("div.ProductCardstyles__TextContainer-l8f8q8-6.fDOdUb a");
+            for (Element element1 : element) {
+                String aLink = element1.attr("abs:href");
+                System.out.println(aLink);
+            }
+
+            //第二个网站PCWorld
+            Document doc1 = Jsoup.parse(new URL("https://www.currys.co.uk/gbuk/iphone-xs/mobile-phones-and-accessories/mobile-phones/362_3412_32041_xx_ba00011553-bv00312946/xx-criteria.html"), 100000);
+            Elements element2 = doc1.select("header.productTitle a");
+            for (Element element1 : element2) {
+                String aLink = element1.attr("href");
+                System.out.println(aLink);
+            }
         }
 
 
-        if(key.equals("SE")) {
-            String IphoneSE = new String("https://www.amazon.co.uk/dp/B0875K11W6/ref=uk_a_phone11_3");
-            return IphoneSE;
+        if(key.equals("IphoneSE")) {
+            //Argos iPhone SE。
+            Document doc = Jsoup.parse(new URL("https://www.argos.co.uk/list/pre-order-new-iphone-se/?tag=ar:search:m020:iphonese-preorder"), 100000);
+            Elements element = doc.select("div.ProductCardstyles__TextContainer-l8f8q8-6.fDOdUb a");
+            for (Element element1 : element) {
+                String aLink = element1.attr("abs:href");
+                System.out.println(aLink);
+            }
+
+            //第二个网站PCWorld
+            Document doc1 = Jsoup.parse(new URL("https://www.currys.co.uk/gbuk/iphone-se/mobile-phones-and-accessories/mobile-phones/362_3412_32041_xx_ba00011553-bv00309808/xx-criteria.html"), 100000);
+            Elements element2 = doc1.select("header.productTitle a");
+            for (Element element1 : element2) {
+                String aLink = element1.attr("href");
+                System.out.println(aLink);
+            }
         }
 
-        if(key.equals("11")) {
-            String Iphone11 = new String("https://www.amazon.co.uk/dp/B07XRNSMW5/ref=uk_a_phone11_2");
-            return Iphone11;
+        if(key.equals("Iphone11")) {
+            //Argos iPhone 11。
+            Document doc = Jsoup.parse(new URL("https://www.argos.co.uk/list/shop-iphone-11?tag=ar:river:apple:iphone11:fromtable:iphone11"), 100000);
+            Elements element = doc.select("div.ProductCardstyles__TextContainer-l8f8q8-6.fDOdUb a");
+            for (Element element1 : element) {
+                String aLink = element1.attr("abs:href");
+                System.out.println(aLink);
+            }
+
+            //第二个网站PCWorld
+            Document doc1 = Jsoup.parse(new URL("https://www.currys.co.uk/gbuk/apple-phones-broadband-and-sat-nav/mobile-phones-and-accessories/mobile-phones/362_3412_32041_267_ba00011553-bv00313386%7Cbv00313387%7Cbv00313391/xx-criteria.html"), 100000);
+            Elements element2 = doc1.select("header.productTitle a");
+            for (Element element1 : element2) {
+                String aLink = element1.attr("href");
+                System.out.println(aLink);
+            }
         }
 
 
 
-        if(key.equals("11pro")) {
-            String Iphone11pro = new String("https://www.amazon.co.uk/dp/B07XRDXNQ5/ref=uk_a_phone11_1");
-            return Iphone11pro;
+        if(key.equals("Iphone11pro")) {
+            //Argos iPhone 11pro。
+            Document doc = Jsoup.parse(new URL("https://www.argos.co.uk/list/shop-iphone-11-pro?tag=ar:river:apple:iphone11:fromtable:iphone11pro"), 100000);
+            Elements element = doc.select("div.ProductCardstyles__TextContainer-l8f8q8-6.fDOdUb a");
+            for (Element element1 : element) {
+                String aLink = element1.attr("abs:href");
+                System.out.println(aLink);
+            }
+
+            //第二个网站PCWorld
+            Document doc1 = Jsoup.parse(new URL("https://www.currys.co.uk/gbuk/apple-iphone-11-pro/mobile-phones-and-accessories/mobile-phones/362_3412_32041_267_ba00011553-bv00313387/xx-criteria.html"), 100000);
+            Elements element2 = doc1.select("header.productTitle a");
+            for (Element element1 : element2) {
+                String aLink = element1.attr("href");
+                System.out.println(aLink);
+            }
         }
 
+
+        if(key.equals("Iphone11proMax")) {
+            //Argos iPhone 11promax。
+            Document doc = Jsoup.parse(new URL("https://www.argos.co.uk/list/shop-iphone-11-pro?tag=ar:river:apple:iphone11:fromtable:iphone11promax"), 100000);
+            Elements element = doc.select("div.ProductCardstyles__TextContainer-l8f8q8-6.fDOdUb a");
+            for (Element element1 : element) {
+                String aLink = element1.attr("abs:href");
+                System.out.println(aLink);
+            }
+
+            //第二个网站PCWorld
+            Document doc1 = Jsoup.parse(new URL("https://www.currys.co.uk/gbuk/apple-iphone-11-pro-max/mobile-phones-and-accessories/mobile-phones/362_3412_32041_267_ba00011553-bv00313391/xx-criteria.html"), 100000);
+            Elements element2 = doc1.select("header.productTitle a");
+            for (Element element1 : element2) {
+                String aLink = element1.attr("href");
+                System.out.println(aLink);
+            }
+        }
+
+
+        if(key.equals("IphoneXSMax")) {
+            //Argos iPhone XS Max。
+            Document doc = Jsoup.parse(new URL("https://www.argos.co.uk/search/iphone-xs-max/?clickOrigin=searchbar:search:term:iphone+xs+max"), 100000);
+            Elements element = doc.select("div.ProductCardstyles__TextContainer-l8f8q8-6.fDOdUb a");
+            for (Element element1 : element) {
+                String aLink = element1.attr("abs:href");
+                System.out.println(aLink);
+            }
+
+            //第二个网站PCWorld
+            Document doc1 = Jsoup.parse(new URL("https://www.currys.co.uk/gbuk/iphone-xs-max/mobile-phones-and-accessories/mobile-phones/362_3412_32041_xx_ba00011553-bv00312947/xx-criteria.html"), 100000);
+            Elements element2 = doc1.select("header.productTitle a");
+            for (Element element1 : element2) {
+                String aLink = element1.attr("href");
+                System.out.println(aLink);
+            }
+        }
+
+
+        if(key.equals("IphoneXR")) {
+            //Argos iPhone XR。
+            Document doc = Jsoup.parse(new URL("https://www.argos.co.uk/list/shop-for-iphone-xr/?tag=ar:apple-iphone-xr:link-1"), 100000);
+            Elements element = doc.select("div.ProductCardstyles__TextContainer-l8f8q8-6.fDOdUb a");
+            for (Element element1 : element) {
+                String aLink = element1.attr("abs:href");
+                System.out.println(aLink);
+            }
+
+            //第二个网站PCWorld
+            Document doc1 = Jsoup.parse(new URL("https://www.currys.co.uk/gbuk/iphone-xr/mobile-phones-and-accessories/mobile-phones/362_3412_32041_xx_ba00011553-bv00313051/xx-criteria.html"), 100000);
+            Elements element2 = doc1.select("header.productTitle a");
+            for (Element element1 : element2) {
+                String aLink = element1.attr("href");
+                System.out.println(aLink);
+            }
+        }
+        return null;
+    }
 
         
         
@@ -181,10 +292,5 @@ public class HTML {
             System.out.println(aLink);
 
         }
-    }
-
-
-        return null;
-
     }
 }
